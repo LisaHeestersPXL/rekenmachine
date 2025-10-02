476c5a76df26f12afca68942bdf764f533e212d9
Lisa-CtrlShiftZ <heesterslisa@gmail.com>
2025-10-02 10:59:04 +0200

    Add project files.

---
+++ rekenmachine/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace rekenmachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+            double solution; 
+            char typeOfOperation;
+            bool validOperation;
+            bool isCorrect = true;
+
+            //Input eerste getal
+            Console.Write("Voer het eerste getal in: ");
+            bool checkFirst = int.TryParse(Console.ReadLine(), out firstNumber);
+            if (!checkFirst)
+            {
+                Console.WriteLine("Foutieve input!");
+            }
+
+            //Input rekendinghoenoemjedat
+            Console.Write("Voer het type operatie in (+, -, *, /): ");
+            validOperation = Char.TryParse(Console.ReadLine(), out typeOfOperation);
+            if (!validOperation)
+            {
+                Console.WriteLine("Foutieve input!"); 
+            }
+
+            //Input tweede getal 
+            Console.Write("Voer het tweede getal in: ");
+            bool checkSecond = int.TryParse(Console.ReadLine(), out secondNumber);
+            if (!checkSecond)
+            {
+                Console.WriteLine("Foutieve input!");
+            }
+
+            //Uitvoering berekening 
+            switch (typeOfOperation)
+            {
+                case '+':
+                    solution = firstNumber + secondNumber;
+                    break;
+                 
+
+                case '-':
+                     solution = firstNumber - secondNumber;
+                    break;
+                   
+                case '*':
+                    solution = firstNumber* secondNumber;
+                    break;
+
+                case '/':
+                    solution = firstNumber / secondNumber;
+                    break;
+
+                default:
+                    solution = 0;
+                    isCorrect = false; 
+                    break;
+
+            }
+
+            if (isCorrect)
+            {
+                Console.WriteLine($"Oplossing is {solution}");
+            } else { 
+                Console.WriteLine("Foutieve berekening!");
+            };
+             
+        }
+    }
+}
